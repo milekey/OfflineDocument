f36b069b65a7a962fb912e07e19dbffe9adeb5d6
milekey <hata@mihr.net>
2023-09-15 17:00:33 +0900

    Modernized build.gradle scripts, etc.

--- app/src/main/java/com/scaredeer/offlinedocument/MainActivity.kt
+++ app/src/main/java/com/scaredeer/offlinedocument/MainActivity.kt
@@ -1,34 +1,37 @@
-package com.scaredeer.offlinedocument;
+package com.scaredeer.offlinedocument
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
-    private static final String OFFLINE_DOCUMENT_ROOT = "file:///android_asset/offline_document";
-    private static final String HOME_DOCUMENT = "index.html";
-    private WebView mWebView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mWebView = new WebView(this);
-        mWebView.setWebViewClient(new WebViewClient());
-        mWebView.loadUrl(OFFLINE_DOCUMENT_ROOT + "/" + HOME_DOCUMENT);
-
-        setContentView(mWebView);
+    companion object {
+        private const val OFFLINE_DOCUMENT_ROOT = "file:///android_asset/offline_document"
+        private const val HOME_DOCUMENT = "index.html"
     }
 
-    @Override
-    public void onBackPressed() {
-        if (mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("$OFFLINE_DOCUMENT_ROOT/$HOME_DOCUMENT")
+
+        setContentView(webView)
+
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        })
     }
 }
